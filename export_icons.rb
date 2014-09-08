c29211da54bbcea4c9c568c5b451350cc4d2e636
ksoichiro <soichiro.kashima@gmail.com>
2014-09-08 22:37:09 +0900

    Bumped version to 1.4.3.

--- export_icons.rb
+++ export_icons.rb
@@ -3,8 +3,8 @@ require "formula"
 class ExportIcons < Formula
   homepage "https://github.com/ksoichiro/export_icons"
 
-  url "https://github.com/ksoichiro/export_icons/archive/v1.4.2.tar.gz"
-  sha1 "ea9dd8c4ab929395e0e3f37ac1a28a93018fe4b9"
+  url "https://github.com/ksoichiro/export_icons/archive/v1.4.3.tar.gz"
+  sha1 "71efd8938ef37a607e709a4c2b8b61f6e2f2ed3a"
 
   def install
     bin.install "export_icons"
